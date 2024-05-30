ca2414460a7cf5943e4ce0fe057d9bf3b3bc7610
Hazel Rella <hazelrella11@gmail.com>
2024-05-30 13:16:36 -0700

    fallible this methods

--- src/cell.rs
+++ src/cell.rs
@@ -20,6 +20,18 @@ where
     T: Cell,
     G: Stainable<Cell = T>,
 {
+    pub fn get_this(&self) -> Result<&T, PowderkegError<T>> {
+        self.grid.get(self.origin)
+    }
+
+    pub fn get_this_mut(&mut self) -> Result<&mut T, PowderkegError<T>> {
+        self.grid.get_mut(self.origin)
+    }
+
+    pub fn get_state(&self) -> Result<Arc<RwLock<T::State>>, PowderkegError<T>> {
+        self.grid.get_state(self.origin)
+    }
+
     pub fn this(&self) -> &T {
         self.grid.at(self.origin)
     }
